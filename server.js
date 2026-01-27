2f00a47ec85633d736842a297fef9f7369e62995
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-27 08:55:48 -0500

    feat: setting up the routes

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from "express";
+import { productsRouter } from "./routes/products.js";
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.static("public"));
+
 /*
-Challenge:
-    1. Use express.static() to serve all the files in 'public'.
+Challenge 2:
+
+- Handle any request to /api/products and pass it to productsRouter.
+
+- Save and reload the mini browser.
+  You should see the results of the console.logs from productsControllers.js
+
 */
 
-app.use(express.static("public"));
+app.use("/api/products", productsRouter);
 
 app
   .listen(PORT, () => {
