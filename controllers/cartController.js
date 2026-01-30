f2e1a20768e9e61d8464867aa2771d3fca4fc05d
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-30 16:54:32 -0500

    feat: show details of cart items

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -70,3 +70,53 @@ Loads of help in hint.md
 
   res.json({ totalItems: result.totalItems || 0 });
 }
+
+export async function getAll(req, res) {
+  // Don't touch this code!
+  if (!req.session.userId) {
+    return res.json({ err: "not logged in" });
+  }
+
+  const db = await getDBConnection();
+
+  /*
+Challenge:
+
+1. When a logged-in user clicks the cart icon, they will be redirected to the cart.html page. To render the user's cart, the frontend needs to get an array of objects similar to the example below when it makes a GET request to the /api/cart endpoint. Important: this array should be sent in a JSON object with the key 'items'.
+
+[
+  {
+    cartItemId: 4,
+    quantity: 2,
+    title: 'Selling Dogma',
+    artist: 'The Clouds',
+    price: 44.99
+  },
+  {
+    cartItemId: 5,
+    quantity: 1,
+    title: 'Midnight Parallels',
+    artist: 'Neon Grove',
+    price: 40.99
+  }
+]
+
+The frontend JS has been done for you.
+
+Ignore frontend console errors for now!
+
+For testing, log in with:
+Username: test
+Password: test
+
+Then click the cart icon to go to the cart page. You should see the user’s items.
+
+Loads of help in hint.md
+*/
+  const productsByUser = await db.all(
+    `SELECT c.id as cartItemId, c.quantity, p.title, p.artist, p.price FROM cart_items c INNER JOIN products p ON c.product_id = p.id WHERE c.user_id = ?`,
+    [req.session.userId],
+  );
+
+  return res.json({ items: productsByUser });
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { addToCart, getCartCount } from "../controllers/cartController.js";
+import {
+  addToCart,
+  getAll,
+  getCartCount,
+} from "../controllers/cartController.js";
 
 export const cartRouter = express.Router();
 
 cartRouter.post("/add", addToCart);
 cartRouter.get("/cart-count", getCartCount);
+cartRouter.get("/", getAll);
